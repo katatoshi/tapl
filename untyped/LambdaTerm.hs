13ae8a564328d9fd955173703016b8384059923c
Katayama-Mac-mini <katayama@Katayama-Mac-mini-no-Mac-mini.local>
2015-11-12 02:53:20 +0900

    ラムダ項の文字列を LambdaTerm 型にパースする関数を実装（未整理）

--- untyped/LambdaTerm.hs
+++ untyped/LambdaTerm.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module LambdaTerm
 ( LambdaTerm
 , isval
@@ -8,6 +10,58 @@ module LambdaTerm
 
 import Control.Monad.Writer
 import Data.Set
+
+-- http://unbui.lt/#!/post/haskell-parsec-basics
+import qualified Text.Parsec as Parsec
+import Text.Parsec ((<?>))
+import Control.Applicative
+import Control.Monad.Identity (Identity)
+
+parse :: Parsec.Stream s Identity t => Parsec.Parsec s () a -> s -> Either Parsec.ParseError a
+parse rule text = Parsec.parse rule "" text
+
+-- BNF はこんな感じ？
+-- ========================================
+-- varName ::= 'a' | ... | 'z'
+--           | varName '\''
+--
+-- term ::= varName
+--        | '(' '\' varName '.' term ')'
+--        | '(' term ' ' term ')'
+-- ========================================
+
+varNameParse :: Parsec.Parsec String () String
+varNameParse = do
+    lowerLetter <- Parsec.lower
+    primes <- Parsec.many (Parsec.char '\'')
+    return (lowerLetter : primes)
+
+tmVarParse :: Parsec.Parsec String () LambdaTerm
+tmVarParse = do
+    varName <- varNameParse
+    return $ TmVar varName
+
+tmAbsParse :: Parsec.Parsec String () LambdaTerm
+tmAbsParse = do
+    Parsec.char '('
+    Parsec.char '\\'
+    varName <- varNameParse
+    Parsec.char '.'
+    term <- lambdaTermParse
+    Parsec.char ')'
+    return $ TmAbs varName term
+
+tmAppParse :: Parsec.Parsec String () LambdaTerm
+tmAppParse = do
+    Parsec.char '('
+    term1 <- lambdaTermParse
+    Parsec.space
+    term2 <- lambdaTermParse
+    Parsec.char ')'
+    return $ term1 `TmApp` term2
+
+lambdaTermParse :: Parsec.Parsec String () LambdaTerm
+lambdaTermParse = Parsec.try tmVarParse <|> Parsec.try tmAbsParse <|> Parsec.try tmAppParse
 
 type VarName = String
 
@@ -77,4 +131,25 @@ evalWithLog t = case eval1 t of
 
 printEvalStep :: LambdaTerm -> IO ()
 printEvalStep t = mapM_ putStrLn $ snd $ runWriter $ evalWithLog t
+
+printEvalStepByGetLine :: IO ()
+printEvalStepByGetLine = do
+    str <- getLine
+    Right term <- return $ parse lambdaTermParse str
+    printEvalStep term
+
+-- パースに失敗したら場合の処理はしていない
+--
+-- このファイルをロードした上で、tapl5-3.pdf の評価の例を ghci 上で次のように確認できる
+--
+-- ghci> let t = parseLambdaTerm "((\\x.x) (\\z.((\\x.x) z)))"
+-- ghci> printEvalStep t
+-- ((\x.x) (\z.((\x.x) z)))
+-- (\z.((\x.x) z))
+--
+-- 値コンストラクタを組み合わせて作るよりは書きやすく読みやすいと思う
+--
+parseLambdaTerm :: String -> LambdaTerm
+parseLambdaTerm str = case parse lambdaTermParse str of
+    Right term -> term
 
